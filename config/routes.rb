289776fbd41db1d7558afa51476ed0f217e92f08
noriko ogihara <aloha.nori1104@gmail.com>
2020-12-23 19:20:12 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'blogs#index'
+  resources :blogs
   if Rails.env.development?
       mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
     end
